92c82e3665b291672e92216c853c2e5d7fa8dcfb
Kris Turner <13710532+kristopherjturner@users.noreply.github.com>
2019-09-03 13:12:15 -0400

    Update AssignUpdateMgmtTag.ps1

--- AssignUpdateMgmtTag.ps1
+++ AssignUpdateMgmtTag.ps1
@@ -6,41 +6,47 @@
 #  Uses the Az Module and not the AzureRM module
 #####################################
 
-#  Need Authentication Step if not running within Azure CloudShell
-#  If not using Cloudshell use the current version of Azure PowerShell
-#  Use the AZ Module and not AzureRM Module
-
-# Variables - Required
+#  Variables - Required
+#  Please fill in the following 4 variables.
 $TenantID=""
 $SubscriptionID=""
+$aztag = ""
+$tagvalue = ""
 
 Connect-AzAccount -Tenant $TenantID -SubscriptionId $SubscriptionID
 
 
 #  Discovery of all Azure VM's in the current subscription.
 $azurevms = Get-AzVM | Select-Object -ExpandProperty Name
-Write-Host "Discovering Azure VM's.  Please hold...."
+Write-Host "Discovering Azure VM's in the following subscription $SubscriptionID  Please hold...."
 
-#  Variables that can change but really shouldn't.  Unless you want to change these variables then they should be changed. 
-$aztag = "UpdateWindow"
-$tagvalue = "Default"
+Write-Host "The following VM's have been discovered in subscription $SubscriptionID"
+$azurevms
+
 
 foreach ($azurevm in $azurevms) {
     
-    Write-Host Adding tag "$aztag" to "$azurevm" with the value of "$tagvalue"
+    Write-Host Checking for tag "$aztag" on "$azurevm"
     $ResourceGroupName = get-azvm -name $azurevm | Select-Object -ExpandProperty ResourceGroupName
     
     $tags = (Get-AzResource -ResourceGroupName $ResourceGroupName `
                         -Name $azurevm).Tags
 
-    $tags.Add($aztag,$tagvalue)
+If ($tags.UpdateWindow){
+Write-Host "$azurevm already has the tag $aztag."
+}
+else
+{
+Write-Host "Creating Tag $aztag and Value $tagvalue for $azurevm"
+$tags.Add($aztag,$tagvalue)
   
     Set-AzResource -ResourceGroupName $ResourceGroupName `
                -ResourceName $azurevm `
                -ResourceType Microsoft.Compute/virtualMachines `
                -Tag $tags `
                -Force `
-               
+   }
+   
 }
 
 Write-Host "All tagging is done (and hopfully it worked).  Please exit the ride to your left.  Have a nice day!"
